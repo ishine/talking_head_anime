c6d9373abf0078c35c247467862ccece53d70bd4
dongho.choi <dongho.choi@mindslab.ai>
2021-12-27 13:30:24 +0900

    import all files with cats, add sample filtering and shape_key extracting code

--- datasets/render.py
+++ datasets/render.py
@@ -1,17 +1,116 @@
 from collections import defaultdict
+import json
 import logging
 import math
 import os
+import pickle
 import random
 
 import numpy as np
 
 import bpy
+import addon_utils
+import mathutils
 
 logging.getLogger("bpy").setLevel(logging.WARNING)
 
-import addon_utils
-import mathutils
+
+# region filtering
+##################
+
+def valid_dirs(dir_root, path_save):
+    models = sorted([os.path.join(dir_root, file) for file in os.listdir(dir_root)
+                     if os.path.isdir(os.path.join(dir_root, file))])
+
+    pmxs = []
+    pmds = []
+    vrms = []
+
+    for dir_model in models:
+        for root, subdirs, files in os.walk(dir_model):
+            for file in files:
+                break_flag = False
+                if file.endswith('.pmx'):
+                    pmxs.append(dir_model)
+                    break_flag = True
+                    break
+                elif file.endswith('.pmd'):
+                    pmds.append(dir_model)
+                    break_flag = True
+                    break
+                elif file.endswith('.vrm'):
+                    vrms.append(dir_model)
+                    break_flag = True
+                    break
+
+                if break_flag:
+                    break
+
+    print(len(models), len(pmxs), len(pmds), len(vrms))
+
+    valid_list = sorted(pmxs + pmds + vrms)
+    valid_list = [os.path.basename(dirname) + '\n' for dirname in valid_list]
+    with open(path_save, 'w', encoding='utf-8') as f:
+        f.writelines(valid_list)
+
+
+def extract_shapekeys(dir_model, path_save):
+    """find and save shape keys and bones(poses)"""
+    # find loadable model
+    path_model = ''
+    for root, subdirs, files in os.walk(dir_model):
+        break_flag = False
+        for file in files:
+            path_model = os.path.join(root, file)
+            if file.endswith('.pmx'):
+                break_flag = True
+                break
+            elif file.endswith('.pmd'):
+                break_flag = True
+                break
+            elif file.endswith('.vrm'):
+                break_flag = True
+                break
+
+        if break_flag:
+            break
+
+    print(path_model)
+    # load model
+    r = Renderer()
+    r.import_model(path_model=path_model)
+
+    data = {key: {} for key in bpy.data.objects.keys()}
+
+    for key, obj in bpy.data.objects.items():
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.select_all(action='DESELECT')  # Deselect all objects
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+
+        # get shape keys
+        if hasattr(obj.data, 'shape_keys') and obj.data.shape_keys is not None:
+            data[key]['shape_keys'] = obj.data.shape_keys.key_blocks.keys()
+            # shape_keys.extend(list(obj.data.shape_keys.key_blocks))
+
+        # get pose(bones)
+        if hasattr(obj, 'pose') and hasattr(obj.pose, 'bones') and obj.pose.bones is not None:
+            bpy.ops.object.select_all(action='DESELECT')  # Deselect all objects
+            bpy.context.view_layer.objects.active = obj
+            obj.select_set(True)
+            bpy.ops.object.mode_set(mode='POSE')
+            data[key]['bones'] = obj.pose.bones.keys()
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        obj.select_set(False)
+
+    # print(lines)
+    with open(path_save, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+
+##################
+# endregion
 
 
 def reset_blender():
@@ -59,6 +158,7 @@ def set_addons():
     ]
     for data in addon_data:
         addon_name = data['name']
+        print(addon_name)
 
         # check addon is installed
         installed_module_names = [module.__name__ for module in addon_utils.modules()]
@@ -92,12 +192,13 @@ import math
 
 
 def import_model(path_input: str):
-    file_extension = path_input.rsplit('.', 1)[-1]
-    if file_extension == 'vrm':
+    file_extension = path_input.rsplit('.', 1)[-1].lower()
+    # if file_extension == 'vrm':
+    if False:
         bpy.ops.import_scene.vrm(filepath=path_input)
-    elif file_extension == 'pmx':
-        # bpy.ops.mmd_tools.import_model(filepath=path_input)
+    elif file_extension == 'pmx' or file_extension == 'pmd' or file_extension == 'vrm':
         bpy.ops.cats_importer.import_any_model(filepath=path_input)
+        # bpy.ops.mmd_tools.import_model(filepath=path_input)
     else:
         raise ValueError(f'file extension {file_extension} not supported')
 
@@ -212,30 +313,74 @@ class Renderer:
         return location
 
 
-if __name__ == '__main__':
+# region test
+
+def test_render():
     r = Renderer()
     path = 'samples/ini-T式雪ノ下雪乃ver100/雪ノ下雪乃ver1.00.pmx'
     path = os.path.join(os.getcwd(), path)
     r.import_model(path)
+    r.set_output_path(os.path.join(os.getcwd(), 'base.png'))
+    r.render()
 
-    # r.set_output_path(os.path.join(os.getcwd(), 'base.png'))
-    # r.render()
-    #
-    # r.change_shapekey('あ', 1.0)
-    # r.set_output_path(os.path.join(os.getcwd(), 'a_1.png'))
-    # r.render()
-    #
-    # r.poseRig(
-    #     bpy.data.objects['雪ノ下雪乃Ver1.00_arm'],
-    #     [
-    #         ('頭', 'Y', 60)
-    #     ]
-    # )
-    # r.set_output_path(os.path.join(os.getcwd(), 'rotate.png'))
-    # r.render()
+    # change shapekey
+    r.change_shapekey('あ', 1.0)
+    r.set_output_path(os.path.join(os.getcwd(), 'a_1.png'))
+    r.render()
 
+    # rotate head
+    r.poseRig(
+        bpy.data.objects['雪ノ下雪乃Ver1.00_arm'],
+        [
+            ('頭', 'Y', 60)
+        ]
+    )
+    r.set_output_path(os.path.join(os.getcwd(), 'rotate.png'))
+    r.render()
+
+    # find head and render
     location = r.find_head_position(bpy.data.objects['雪ノ下雪乃Ver1.00_mesh'])
     bpy.data.objects['camera'].location = mathutils.Vector(location + (0, -1, 0))
     bpy.data.objects['camera'].rotation_euler = mathutils.Euler((math.pi / 2., 0, 0))
     r.set_output_path(os.path.join(os.getcwd(), 'head.png'))
     r.render()
+
+
+# endregion
+
+if __name__ == '__main__':
+    import sys
+
+    # r = Renderer()
+
+    idx_file = 'metadata/nicovideo.txt'
+    with open(idx_file, 'r', encoding='utf-8') as f:
+        model_idxs = f.readlines()
+    model_idx = model_idxs[int(sys.argv[-1])].strip()
+    # dir_model = os.path.join(os.getcwd(), 'samples', model_idx)
+    dir_root = '/raid/vision/dhchoi/data/3d_models/models'
+    dir_model = os.path.join(dir_root, model_idx)
+    if os.path.exists(dir_model):
+        try:
+            print(dir_model)
+            path_save = os.path.join('./', 'metadata', 'shape_keys', f'{model_idx}.json')
+            os.makedirs(os.path.dirname(path_save), exist_ok=True)
+            extract_shapekeys(dir_model, path_save)
+        except Exception as e:
+            # raise e
+            print(e)
+            pass
+
+    print('###########finished############')
+
+    # r.set_output_path(os.path.join(os.getcwd(), model_idx + '.png'))
+    # obj = [value for key, value in bpy.data.objects.items() if 'mesh' in key][0]  # TODO this should be fixed, not works for all model
+    # location = r.find_head_position(obj)
+    # bpy.data.objects['camera'].location = mathutils.Vector(location + (0, -1, 0))
+    # bpy.data.objects['camera'].rotation_euler = mathutils.Euler((math.pi / 2., 0, 0))
+    # bpy.data.objects['light'].location = mathutils.Vector(location + (0, -1.5, 0))
+    # bpy.data.lights['light'].energy = 100
+    #
+    # bpy.context.scene.render.resolution_x = 512
+    # bpy.context.scene.render.resolution_y = 512
+    # r.render()
