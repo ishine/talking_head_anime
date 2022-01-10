f66e64781a0fcfb494eaa6450b21c643367e7abe
dongho.choi <dongho.choi@mindslab.ai>
2022-01-11 01:27:49 +0900

    lil refactoring to renderer

--- datasets/render.py
+++ datasets/render.py
@@ -15,111 +15,109 @@ import pyvirtualdisplay
 logging.getLogger("bpy").setLevel(logging.WARNING)
 
 
-def reset_blender():
-    bpy.ops.wm.read_factory_settings(use_empty=True)
-
-
-def clean_blender():
-    for i in range(10):
-        for attribute in dir(bpy.data):
-            try:
-                bpy_dataset = getattr(bpy.data, attribute)
-                for key, value in bpy_dataset.items():
-                    if key != 'camera' and key != 'light' and key != 'Scripting' and not (
-                            attribute == 'texts' and key == 'render.py'):
-                        bpy_dataset.remove(value)
-            except Exception as e:
-                pass
-
-
-def build_camera_light():
-    if 'camera' not in bpy.context.scene.objects.keys():
-        cam_data = bpy.data.cameras.new('camera')
-        cam = bpy.data.objects.new('camera', cam_data)
-        bpy.context.collection.objects.link(cam)
-        bpy.context.scene.camera = cam
-
-    if 'light' not in bpy.context.scene.objects.keys():
-        light_data = bpy.data.lights.new('light', type='POINT')
-        # light_data = bpy.data.lights.new('light', type='SUN')
-        light = bpy.data.objects.new('light', light_data)
-        bpy.context.collection.objects.link(light)
-
-
-def set_addons():
-    addon_data = [
-        # {'name': 'VRM_Addon_for_Blender-release',
-        #  'url': '',
-        #  'path': './addons/VRM_Addon_for_Blender-release.zip', },
-        # {'name': 'mmd_tools',
-        #  'url': '',
-        #  'path': './addons/mmd_tools-v1.0.1.1.zip', },
-        {'name': 'cats-blender-plugin-master',
-         'url': '',
-         'path': './addons/cats-blender-plugin-master.zip', },
-    ]
-    for data in addon_data:
-        addon_name = data['name']
-        print(addon_name)
-
-        # check addon is installed
-        installed_module_names = [module.__name__ for module in addon_utils.modules()]
-        if not addon_name in installed_module_names:
-            addon_path = data['path']
-            bpy.ops.preferences.addon_install(overwrite=True, target='DEFAULT', filepath=addon_path)
-
-        # check addon is loaded
-        is_loaded = addon_utils.check(addon_name)
-        if not is_loaded[0] and not is_loaded[1]:
-            addon_utils.enable(addon_name)
-
-
-def render_settings():
-    bpy.data.objects['light'].location = mathutils.Vector((0, -10, 0))
-    bpy.data.lights['light'].energy = 100
-
-    bpy.data.objects['camera'].location = mathutils.Vector((0, -5, 0))
-    bpy.data.objects['camera'].rotation_euler = mathutils.Euler((1.5708, 0, 0))
-
-    bpy.context.scene.render.image_settings.file_format = 'PNG'
-
-    # engine choosing: https://www.cgdirector.com/best-renderers-render-engines-for-blender/
-    bpy.context.scene.render.engine = 'BLENDER_EEVEE'
-    # bpy.context.scene.render.engine = 'CYCLES'
-
-
-def import_model(path_input: str):
-    file_extension = path_input.rsplit('.', 1)[-1].lower()
-    # if file_extension == 'vrm':
-    if False:
-        bpy.ops.import_scene.vrm(filepath=path_input)
-    elif file_extension == 'pmx' or file_extension == 'pmd' or file_extension == 'vrm':
-        bpy.ops.cats_importer.import_any_model(filepath=path_input)
-        # bpy.ops.mmd_tools.import_model(filepath=path_input)
-    else:
-        raise ValueError(f'file extension {file_extension} not supported')
-
-
 class Renderer:
     def __init__(self):
-        clean_blender()
-        build_camera_light()
-        set_addons()
-        render_settings()
+        self.clean_blender()
+        self.set_addons()
+        self.init_camera()
+        self.init_light()
+        self.set_configs()
+
         self.display = pyvirtualdisplay.Display()
         self.display.start()
 
         self.current_model = ''
 
+    # region settings
+
+    @staticmethod
+    def set_configs():
+        bpy.context.scene.render.image_settings.file_format = 'PNG'
+
+        # engine choosing: https://www.cgdirector.com/best-renderers-render-engines-for-blender/
+        bpy.context.scene.render.engine = 'BLENDER_EEVEE'
+
+    @staticmethod
+    def set_addons():
+        addon_data = [
+            {'name': 'cats-blender-plugin-master',
+             'url': '',
+             'path': './addons/cats-blender-plugin-master.zip', },
+        ]
+        for data in addon_data:
+            addon_name = data['name']
+            print(addon_name)
+
+            # check addon is installed
+            installed_module_names = [module.__name__ for module in addon_utils.modules()]
+            if not addon_name in installed_module_names:
+                addon_path = data['path']
+                bpy.ops.preferences.addon_install(overwrite=True, target='DEFAULT', filepath=addon_path)
+
+            # check addon is loaded
+            is_loaded = addon_utils.check(addon_name)
+            if not is_loaded[0] and not is_loaded[1]:
+                addon_utils.enable(addon_name)
+
+    @staticmethod
+    def init_camera():
+        if 'camera' not in bpy.context.scene.objects.keys():
+            cam_data = bpy.data.cameras.new('camera')
+            cam = bpy.data.objects.new('camera', cam_data)
+            bpy.context.collection.objects.link(cam)
+            bpy.context.scene.camera = cam
+        bpy.data.objects['camera'].location = mathutils.Vector((0, 0, 0))
+        bpy.data.objects['camera'].rotation_euler = mathutils.Euler((math.pi / 2, 0, 0))
+
+    @staticmethod
+    def init_light():
+        if 'light' not in bpy.context.scene.objects.keys():
+            light_data = bpy.data.lights.new('light', type='POINT')
+            # light_data = bpy.data.lights.new('light', type='SUN')
+            light = bpy.data.objects.new('light', light_data)
+            bpy.context.collection.objects.link(light)
+        bpy.data.objects['light'].location = mathutils.Vector((0, -10, 0))
+        bpy.data.lights['light'].energy = 100
+
+    def exit(self):
+        if self.display.is_alive():
+            self.display.stop()
+        self.clean_blender()
+        bpy.ops.wm.read_factory_settings(use_empty=True)
+
+    # endgreion
+
+    @staticmethod
+    def _import_model(path_input: str):
+        file_extension = path_input.rsplit('.', 1)[-1].lower()
+        if file_extension == 'pmx' or file_extension == 'pmd' or file_extension == 'vrm':
+            bpy.ops.cats_importer.import_any_model(filepath=path_input)
+        else:
+            raise ValueError(f'file extension {file_extension} not supported')
+
     def import_model(self, path_model: str):
+        path_model = os.path.abspath(path_model)
         if self.current_model != path_model:
             print('deleting old model and loading new model')
-            clean_blender()
-            import_model(path_model)
+            self.clean_blender()
+            self._import_model(path_input=path_model)
+
             self.current_model = path_model
 
-    def clear(self):
-        clean_blender()
+    @staticmethod
+    def clean_blender():
+        for i in range(10):
+            for attribute in dir(bpy.data):
+                try:
+                    bpy_dataset = getattr(bpy.data, attribute)
+                    for key, value in bpy_dataset.items():
+                        if key != 'camera' and key != 'light' and key != 'Scripting' and not (
+                                attribute == 'texts' and key == 'render.py'):
+                            bpy_dataset.remove(value)
+                except Exception as e:
+                    pass
+
+    # region render
 
     def render_complex(self, path_output, parameters=None):
         self.set_output_path(path_output)
@@ -166,6 +164,10 @@ class Renderer:
         # copy buffer to numpy array for faster manipulation
         arr = np.array(pixels[:])
         print(arr.shape)
+
+    # endgreion
+
+    # region utils
 
     def change_shapekey(self, key, value):
         # find first object which has given key
@@ -214,10 +216,7 @@ class Renderer:
 
         return location
 
-    def exit(self):
-        self.display.stop()
-        self.clear()
-        reset_blender()
+    # endregion
 
 
 # region test
