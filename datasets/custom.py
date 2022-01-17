e2caa5bd9b9585ada5c2248361c29ec00c473560
dongho.choi <dongho.choi@mindslab.ai>
2022-01-17 17:51:30 +0900

    now subprocessdataset generates target image

--- datasets/custom.py
+++ datasets/custom.py
@@ -1,6 +1,7 @@
 import os
 import random
 import subprocess
+import time
 
 import cv2
 import torch
@@ -24,23 +25,44 @@ class SubprocessDataset(BaseDataset):
         return torch.from_numpy(img).permute((2, 0, 1)) / 255.
 
     def __getitem__(self, idx):
+        return_data = {}
+
+        key_mouth = 'あ'
+        val_mouth = random.random()
+        val_mouth = float(f'{val_mouth:.04f}')
+
+        key_left_eye = 'ウィンク'
+        val_left_eye = random.random()
+        val_left_eye = float(f'{val_left_eye:.04f}')
+
+        key_right_eye = 'ウィンク右'
+        val_right_eye = random.random()
+        val_right_eye = float(f'{val_right_eye:.04f}')
+
+        return_data['pose'] = torch.FloatTensor([val_mouth, val_left_eye, val_right_eye])
+
         commands = [
             'python', '-m', 'datasets.script2',
-            f'{self.conf.path["metadata"]}',
-            f'{idx}',
+            f'{self.conf.path["metadata"]}', f'{idx}',
+            f'{key_mouth}___{val_mouth}',
+            f'{key_left_eye}___{val_left_eye}',
+            f'{key_right_eye}___{val_right_eye}',
         ]
         command = ' '.join(commands)
         subprocess.call(command, shell=True, stdout=open(os.devnull, 'wb'))
 
-        return_data = {}
-
         tmp_dir = self.conf.path['tmp']
-        tmp_path = os.path.join(tmp_dir, f'{idx}.png')
+
+        tmp_path = os.path.join(tmp_dir, f'{idx}_{val_mouth}_{val_left_eye}_{val_right_eye}.png')
         while not os.path.exists(tmp_path):
             time.sleep(0.5)
             print('waiting', tmp_path)
-        img_base_np = cv2.imread(tmp_path, cv2.IMREAD_UNCHANGED)
+        img_target = cv2.imread(tmp_path, cv2.IMREAD_UNCHANGED)
         os.remove(tmp_path)
+        return_data['img_target'] = self.np_img_to_torch(img_target)
+
+        tmp_path = os.path.join(tmp_dir, f'{idx}.png')
+        img_base_np = cv2.imread(tmp_path, cv2.IMREAD_UNCHANGED)
         return_data['img_base'] = self.np_img_to_torch(img_base_np)
 
         return return_data
@@ -65,7 +87,6 @@ class PlaceholderDataset(BaseDataset):
 
 if __name__ == '__main__':
     from omegaconf import OmegaConf
-    import time
     from torch.utils.data import DataLoader
     from utils.util import cycle
 
@@ -82,5 +103,5 @@ if __name__ == '__main__':
 
     from tqdm import trange
 
-    for i in trange(10):
+    for i in trange(20):
         item = next(it)
--- datasets/script2.py
+++ datasets/script2.py
@@ -100,30 +100,34 @@ def main():
 
     # with pyvirtualdisplay.Display(visible=False, size=(1,1)) as disp:
     if True:
+        # set renderer
         r = Renderer(make_display=False)
         r.set_addons()
         r.import_model(model_path)
         r.set_camera_position()
 
+        # render rest-pose image
         tmp_dir = '/raid/vision/dhchoi/data/3d_models/tmp'
         temp_path = os.path.join(tmp_dir, f'{internal_idx}.png')
         if not os.path.exists(temp_path):
             r.set_output_path(temp_path)
             r.render()
 
+        # render moved image
+        temp_path = os.path.join(tmp_dir, f'{internal_idx}')
+        for item in sys.argv[3:]:
+            key, value = item.strip().split('___')
+            value = float(value)
+            r.change_shapekey(key, value)
+            temp_path += f'_{value}'
+
+        # temp_path = sys.argv[-1]
+        temp_path += '.png'
+        r.set_output_path(temp_path)
+        r.render()
+
         # disp.stop()
 
-    # temp_path = os.path.join(tmp_dir, f'{internal_idx}')
-    # for item in sys.argv[2:]:
-    #     key, value = item.strip().split('___')
-    #     value = float(value)
-    #     r.change_shapekey(key, value)
-    #     temp_path += f'_{value}'
-    #
-    # # temp_path = sys.argv[-1]
-    # temp_path += '.png'
-    # r.set_output_path(temp_path)
-    # r.render()
     r.exit()
 
 
